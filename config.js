75a563a27c92abb77b7cb684284d7ea44e466ac1
galiprandi <galiprandi@gmail.com>
2017-09-30 02:21:05 -0300

    Primer commit

---
+++ config.js
@@ -0,0 +1,22 @@
+var data = {
+// Datos del Evento
+  eNombre: 'Boda de Horacio y Lucia',
+  eUbicacion: 'Salón Las Moras',
+  eFecha: '26/09/2017',
+  eFotos: 11,         // Cantidad de Fotos
+
+  // Datos del Fotógrafo
+  fNombre: 'Germán Aliprandi',
+  fWeb: 'http://wwww.gadget.com',
+  fMail: 'galiprandi@gmail.com',
+  fTel: '381 5900868',
+
+  // Configuración Principal
+  debug: true,                      // Envía información a consola
+  pausePlay: false,                   // Pausa el reproductor (true, false)
+  timeInterval: 6000,                 // Tiempo entre Fotos en milisegungos def: 7000
+  imgPath: 'imgs/',                 // Ruta donde se encuentran las imágenes
+  imgType: '.jpg',                    // Tipo de imágenes
+  imgFrame: true                  // Marco en las imágenes
+
+}
